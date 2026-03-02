ede4f6114645189f561b10a653f9d5cec531c081
yakoltsevich <tpaktap1221@gmail.com>
2026-03-02 16:42:32 +0100

    .

--- src/components/happenings/HappeningsCard.tsx
+++ src/components/happenings/HappeningsCard.tsx
@@ -4,7 +4,7 @@ import React, {ReactNode} from "react";
 
 export const HappeningsCard = ({children}: { children: ReactNode }) => {
     return (
-        <div className="group relative overflow-hidden w-full  shadow-[0_18px_70px_rgba(0,0,0,0.65)]">
+        <div className="group relative overflow-hidden w-full  shadow-[0_18px_70px_rgba(0,0,0,0.65)] rounded-[22px]">
             <div
                 className="relative overflow-hidden rounded-[22px] border border-white/10 bg-black/25 transition hover:border-white/15 ">
                 {/* background image */}
